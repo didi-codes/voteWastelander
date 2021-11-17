a2ebafc93505273b8de94c0925368063d6de9a7c
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-11-17 09:42:03 -0500

    section component complete maps over wastelander database to display results of votes...it is being imported into the results page

---
+++ wastelander-winner/components/Section.js
@@ -0,0 +1,29 @@
+const Section = ({ wastelanders }) => {
+  return (
+    <div className='section'>
+      <div>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Costume</th>
+            <th>Originality</th>
+            <th>Creativity</th>
+            <th>Wisdom</th>
+          </tr>
+
+          {wastelanders.map((wastelander) => (
+            <tr>
+              <td>{wastelander.name}</td>
+              <td>{wastelander.costume}</td>
+              <td>{wastelander.originality}</td>
+              <td>{wastelander.creativity}</td>
+              <td>{wastelander.wisdom}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
--- wastelander-winner/pages/results.js
+++ wastelander-winner/pages/results.js
@@ -1,54 +1,50 @@
 import { gql, GraphQLClient } from 'graphql-request';
+import Nav from '../components/Nav';
+import Section from '../components/Section';
 
 export const getStaticProps = async () => {
-    const url = process.env.ENDPOINT
-    const graphQLClient = new GraphQLClient(url, {
-        headers: {
-            "Authorization": `Bearer ${process.env.GRAPH_CMS_TOKEN}`
-        },
-    });
+  const url = process.env.ENDPOINT;
+  const graphQLClient = new GraphQLClient(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.GRAPH_CMS_TOKEN}`,
+    },
+  });
 
-    const wastelanderQuery = gql`
+  const wastelanderQuery = gql`
     query {
-        wastelanders {
-            id
-            name
-            costume
-            creativity
-            originality
-            wisdom
-            }
+      wastelanders {
+        id
+        name
+        costume
+        creativity
+        originality
+        wisdom
+      }
     }
-    `;
+  `;
 
-    const data = await graphQLClient.request(wastelanderQuery)
-    const wastelanders = data.wastelanders;
-    console.log(wastelanders)
+  const data = await graphQLClient.request(wastelanderQuery);
+  const wastelanders = data.wastelanders;
+  console.log(wastelanders);
 
-    return {
-        props: {
-            wastelanders,
-        },
-    };
+  return {
+    props: {
+      wastelanders,
+    },
+  };
 };
 
-const Results = ({wastelanders}) => {
-    return (
-        <div>
-            <h1>Results</h1>
-            <div>
-                <ul>
-                {wastelanders.map(wastelander => {
-                    
-                    <li>{wastelander.name}</li>
-                })}
-                </ul>
-            </div>
-            <div>
-             
-            </div>
-        </div>
-    )
-}
+const Results = ({ wastelanders }) => {
+  return (
+    <div>
+     <Nav />
+      <div>
+        <h1>Results</h1>
+        <Section wastelanders={wastelanders} />
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Results
+export default Results;
